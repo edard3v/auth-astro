3ad20e9bbece7f96f7294d7e0181bfbe03d0e96c
edard3v <edard3v@gmail.com>
2025-02-25 04:52:11 -0500

    agg integraciones db y netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), db()],
+  adapter: netlify(),
+  output: "server",
 });
